aa3035dfa18e3f8193f7ed811941877c8158ab9e
PingleiHE <pingleihe@outlook.com>
2021-05-04 11:15:24 +0800

    Added jQuery folder and the starting skeleton of the Simon Game

---
+++ jQuery/index.js
@@ -0,0 +1,25 @@
+/*
+$("h1").text("Hey Angela");
+
+$("button").html("<a>Click Me</a>");
+$("a").attr("href", "https://www.google.com");
+$('a').on("click", function () {
+    alert("Hey you clicked me");
+});
+$("input").on("keydown", function (event) {
+    console.log(event.key);
+})
+
+*/
+
+/*
+$("html").on("keydown", function (e) {
+    $("h1").text(e.key);
+})
+*/
+
+$("button").html("Click me to have more LUUV");
+$("button").on("click", function () {
+    $("h1").append(" LUUV ");
+    $("h1").append("<img src = 'HelenSchtuSchtu.png' alt='HelenSchtewSchtew' style='height:60px;'>");
+})
